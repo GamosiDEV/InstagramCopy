cd1676cff0593e1a16935276fc2ad56d483f035f
Gabriel de Moura Silva <gm.silva111297@gmail.com>
2022-04-26 18:03:18 -0300

    Prototype of feed screen created, firebase configured and working - Protótipo da tela de feed criado, Firebase configurado e funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main() async {
+  runApp(ModularApp(module: AppModule(), child: AppWidget()));
+
+
+}
